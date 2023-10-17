e9b177fc7f6b739e61da3ba4b6a29caca4b82f69
ashwini-deshpande <ashwini.deshpande@talentica.com>
2023-10-17 07:57:31 +0530

    AuthPOC

---
+++ resourrceserver/src/main/java/com/sfa/resourrceserver/ResourrceserverApplication.java
@@ -0,0 +1,13 @@
+package com.sfa.resourrceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourrceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourrceserverApplication.class, args);
+	}
+
+}
